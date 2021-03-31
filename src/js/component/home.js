3709263d4907d76f5470643520886bea80479f48
edianibarrola <edianibarrola@gmail.com>
2021-03-31 01:01:44 +0000

    basic functions working

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,55 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
 	const [inputValue, setInputValue] = useState("");
+	const [myList, setMyList] = useState([]);
 	const handleInputChange = input => {
 		setInputValue(input);
 	};
+	const addToList = e => {
+		if (e.key == "Enter" && inputValue != null) {
+			setMyList(
+				myList.concat({
+					label: inputValue,
+					done: false
+				})
+			);
+			console.log(myList);
+			setInputValue("");
+		}
+	};
 	return (
-		<input
-			type="text"
-			onChange={e => handleInputChange(e.target.value)}
-			value={inputValue}></input>
+		<div>
+			<div className="listContainer">
+				<h1>Wife-I</h1>
+				<h3>
+					{myList.length > 0
+						? "Do these things before she gets mad"
+						: "Your wife is waiting to give you tasks..."}
+				</h3>
+				<input
+					type="text"
+					onChange={e => handleInputChange(e.target.value)}
+					value={inputValue}
+					onKeyPress={e => addToList(e)}></input>
+
+				<ul className="list-unstyled">
+					{myList.map((item, index) => (
+						<li key={index}>
+							{item.label}
+							<button className="deleteIcon">X</button>
+						</li>
+					))}
+					<li className="text-center">
+						<h4 className="mx-auto">
+							{myList.length > 0
+								? `${myList.length} thing(s) left to do...`
+								: "She's waiting..."}
+						</h4>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
 }
